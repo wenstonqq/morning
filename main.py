2909bbb6bdad76ae009111fb952b3cfc22794ac8
Winston <704847479@qq.com>
2023-04-21 10:40:43 +0800

    Update main.py
    温度描述信息更新

--- main.py
+++ main.py
@@ -48,6 +48,6 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperatureHigh, temperatureLow = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":"最低气温："+temperatureLow+",最高气温："+temperatureHigh},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"weather":{"value":wea},"temperature":{"value":"最低气温："+temperatureLow+"℃，最高气温："+temperatureHigh},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
